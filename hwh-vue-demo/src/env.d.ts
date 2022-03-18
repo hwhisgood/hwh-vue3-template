f76b6a76ab3b9b3e2ba93e8a45e85c9e9c033c28
weihang <huangweihang@lvshou.com>
2022-03-18 15:33:16 +0800

    fix:文件目录错位

--- hwh-vue-demo/src/env.d.ts
+++ hwh-vue-demo/src/env.d.ts
@@ -1,5 +1,6 @@
 /// <reference types="vite/client" />
 
+// 帮助编译器识别类型。
 declare module '*.vue' {
   import type { DefineComponent } from 'vue'
   // eslint-disable-next-line @typescript-eslint/no-explicit-any, @typescript-eslint/ban-types
